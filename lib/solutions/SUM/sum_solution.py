6b35c67ad221077968b2d40f0b57c8657153bf2a
sbx_user1051 <sbx_user1051@169.254.19.145>
2022-02-23 21:44:52 +0000

    Wed Feb 23 21:44:52 UTC 2022

--- lib/solutions/SUM/sum_solution.py
+++ lib/solutions/SUM/sum_solution.py
@@ -1,8 +1,5 @@
 # noinspection PyShadowingBuiltins,PyUnusedLocal
 def compute(x, y):
-    import random
-    x=random.randint(0,100)
-    y=random.randint(0,100)
-    return x+y
+    return sum(x,y)
 
 
